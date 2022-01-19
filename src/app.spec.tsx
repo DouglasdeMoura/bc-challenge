00a6dc8e004c17c88a87384fc23557286ccba772
Douglas Moura <douglas.ademoura@gmail.com>
2022-01-18 22:20:57 -0300

    feat(App): add hero

--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -5,7 +5,10 @@ import { App } from './app'
 describe('<App />', () => {
   it('should render the welcome message', () => {
     render(<App />)
+    const title = screen.getByText('Search')
+    const subtitle = screen.getByText('Find the best GIFs')
 
-    expect(screen.getByText('Hello, World!')).toBeInTheDocument()
+    expect(title).toHaveClass('title')
+    expect(subtitle).toHaveClass('subtitle')
   })
 })
--- src/app.tsx
+++ src/app.tsx
@@ -3,5 +3,14 @@ import { FC } from 'react'
 import 'bulma/css/bulma.css'
 
 export const App: FC = () => {
-  return <h1>Hello, World!</h1>
+  return (
+    <>
+      <div className="hero">
+        <div className="hero-body">
+          <h1 className="title">Search</h1>
+          <p className="subtitle">Find the best GIFs</p>
+        </div>
+      </div>
+    </>
+  )
 }
